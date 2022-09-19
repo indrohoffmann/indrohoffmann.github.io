29e7b6d2c17e5f4792b3962db0e0ef451c361432
indrohoffmann <69474676+indrohoffmann@users.noreply.github.com>
2022-09-19 18:43:55 +0300

    fix no created lottos display

--- myLotterys.js
+++ myLotterys.js
@@ -1,43 +1,60 @@
 import * as firebase from "./firebase.js"
 
+const auth = await firebase.getAuth(firebase.app);
+let userCurrent = await auth.currentUser;
+
+
+firebase.onAuthStateChanged(auth, (user) => {
+    if (user) {
+        userCurrent = user
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/firebase.User
+    }
+});
 
 
 const querySnapshot = await firebase.getDocs(firebase.collection(firebase.db, "Lottos"));
-querySnapshot.forEach((doc) => {
+
+const filterdLottos = querySnapshot.docs.filter((doc) => {
+    return userCurrent.uid == doc.data().creator
+})
+
+const createLottoBtnMylotteryPage = document.getElementById("createLottoBtnMylotteryPage");
+
+createLottoBtnMylotteryPage.onclick = function (e) {
+    e.preventDefault(); //Preventing page refresh after signIn button pressed
+    window.location.replace("createnewlotto.html");
+};
+
+filterdLottos.forEach((doc) => {
     //siia et creatiks uue elemendi kuidas doc datast saab nime end date jne.
-    const auth = firebase.getAuth();
-    const user = auth.currentUser;
-    const createLottoBtnMylotteryPage = document.getElementById("createLottoBtnMylotteryPage");
 
-    createLottoBtnMylotteryPage.onclick = function (e) {
-        e.preventDefault(); //Preventing page refresh after signIn button pressed
-        window.location.replace("createnewlotto.html");
-
-    };
 
     //kontrollib et oleksid nähtaval ainult kasutaja omad lotod
-    if (user.uid == doc.data().creator) {
-        const myCreatedLotterys = document.getElementById("myCreatedLotterys");
-        let newListElement = document.createElement('li');
-        newListElement.classList.add("list-group-item")
-        let newListItem = document.createElement('a');
-        //doc.data on siis tabeli sisu
-        newListItem.textContent = doc.data().lotterysName;
 
-        //et lotto nimed olekss lingid ja lisab lingile parameetri ID kaasa
-        newListItem.href = "lotto.html?ID=" + doc.id;
+    const myCreatedLotterys = document.getElementById("myCreatedLotterys");
+    let newListElement = document.createElement('li');
+    newListElement.classList.add("list-group-item")
+    let newListItem = document.createElement('a');
+    //doc.data on siis tabeli sisu
+    newListItem.textContent = doc.data().lotterysName;
 
-        newListElement.appendChild(newListItem);
-        myCreatedLotterys.appendChild(newListElement);
+    //et lotto nimed olekss lingid ja lisab lingile parameetri ID kaasa
+    newListItem.href = "lotto.html?ID=" + doc.id;
 
-        console.log(`${doc.id} => ${doc.data().toString()}`);
+    newListElement.appendChild(newListItem);
+    myCreatedLotterys.appendChild(newListElement);
 
-        document.getElementById('myCreatedLotterysNotCreatedYet').style.display = 'none';
+    console.log(`${doc.id} => ${doc.data().toString()}`);
 
-    } else {
-
-        document.getElementById('myCreatedLotterysNotCreatedYet').style.display = 'block';
-    }
-
+    document.getElementById('myCreatedLotterysNotCreatedYet').style.display = 'none';
 
 });
+
+if (filterdLottos.length < 1) {
+    document.getElementById('myCreatedLotterysNotCreatedYet').style.display = 'block';
+}
+
+
+
+console.log(filterdLottos)
